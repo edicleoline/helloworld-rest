2d58e4bf6c4b1ddbfd2a6cfac2952cf966b4d11c
edicleoline <edicleo@proton.me>
2024-09-27 07:37:35 -0300

    Update entities

--- helloworld/rest/api/routes/authentication.py
+++ helloworld/rest/api/routes/authentication.py
@@ -1,23 +1,33 @@
 from __future__ import annotations
 
+from pydantic import BaseModel
 from fastapi import APIRouter
 
-from helloworld.auth.features.authentication import get_identify_use_case, get_authenticate_use_case, ResponseEntity
+from helloworld.auth.features.authentication import get_identify_use_case, get_authenticate_use_case, AuthenticateResponseEntity, IdentifyResponseEntity
 from helloworld.account.features.user import UserEntity
 
 router = APIRouter()
 
-@router.post("/auth/identify")
-async def identity(identifier: str) -> str | None:
-    identify_use_case = await get_identify_use_case()
-    token = await identify_use_case.execute(identifier=identifier)
+class IdentityRequest(BaseModel):
+    identifier: str
 
-    return token
+class AuthenticateRequest(BaseModel):
+    token: str
+    password: str | None
+    user: UserEntity | None
+
+
+@router.post("/auth/identify")
+async def identity(body: IdentityRequest) -> IdentifyResponseEntity | None:
+    identify_use_case = await get_identify_use_case()
+    response = await identify_use_case.execute(identifier=body.identifier)
+
+    return response
 
 @router.post("/auth/access-token")
-async def authenticate(token: str, password: str | None = None, user: UserEntity | None = None) -> ResponseEntity | None:
+async def authenticate(body: AuthenticateRequest) -> AuthenticateResponseEntity | None:
     authenticate_use_case = await get_authenticate_use_case()
-    response = await authenticate_use_case.execute(token=token, password=password, user=user)
+    response = await authenticate_use_case.execute(token=body.token, password=body.password, user=body.user)
 
     return response
 
